e99ff167f838394b877da87fc5b74e62783eeb8a
rafael <rppereira@tjma.jus.br>
2020-11-19 14:49:47 -0300

    :bug: Inicializar correção e formatação das datas

--- app/src/main/java/com/br/tmchickendistributor/util/DateUtils.java
+++ app/src/main/java/com/br/tmchickendistributor/util/DateUtils.java
@@ -6,62 +6,54 @@ import java.util.Date;
 
 public class DateUtils {
 
-    private static final SimpleDateFormat FORMATADOR_dd_MM_YYYY_hh_mm =
-            new SimpleDateFormat("dd/MM/yyyy hh:mm");
+  private static final SimpleDateFormat DD_MM_YYYY_HH_MM = new SimpleDateFormat("dd/MM/yyyy hh:mm");
 
-    private static final SimpleDateFormat FORMATADOR_dd_MM_YYYY =
-            new SimpleDateFormat("dd/MM/yyyy");
-    private static final SimpleDateFormat FORMATADOR_YYYY_MM_DD_HH_MM_SS =
-            new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
+  private static final SimpleDateFormat DD_MM_YYYY = new SimpleDateFormat("dd/MM/yyyy");
 
-    public static String formatarDateddMMyyyyParaString(java.util.Date dateToFormat) {
+  private static final SimpleDateFormat YYYY_MM_DD_HH_MM_SS =
+      new SimpleDateFormat("EEE MMM dd HH:mm:ss z yyyy");
 
-        return FORMATADOR_dd_MM_YYYY.format(dateToFormat);
-    }
+  public static String formatarDateddMMyyyyParaString(java.util.Date dateToFormat) {
 
-    public static java.util.Date formatarDateParaddMMyyyyhhmm(java.util.Date dateToFormat)
-            throws ParseException {
+    return DD_MM_YYYY.format(dateToFormat);
+  }
 
+  public static java.util.Date formatarDateParaddMMyyyyhhmm(java.util.Date dateToFormat)
+      throws ParseException {
 
-        String strDate = FORMATADOR_dd_MM_YYYY_hh_mm.format(dateToFormat);
+    String strDate = DD_MM_YYYY_HH_MM.format(dateToFormat);
 
-        return FORMATADOR_dd_MM_YYYY_hh_mm.parse(strDate);
-    }
+    return DD_MM_YYYY_HH_MM.parse(strDate);
+  }
 
-    public static String converterDateParaStringYYYYMMDDHHMM(java.util.Date dateToFormat)
-            throws ParseException {
+  public static String converterDateParaStringYYYYMMDDHHMM(java.util.Date dateToFormat)
+      throws ParseException {
 
-        String dataFormatada=FORMATADOR_dd_MM_YYYY_hh_mm.format(dateToFormat);
-        return dataFormatada;
+    String dataFormatada = DD_MM_YYYY_HH_MM.format(dateToFormat);
+    return dataFormatada;
+  }
 
+  public static String formatarParaYYYYMMDDHHMMSS(java.util.Date dateToFormat) {
 
-    }
+    return YYYY_MM_DD_HH_MM_SS.format(dateToFormat);
+  }
 
-    public static String formatarParaYYYYMMDDHHMMSS(java.util.Date dateToFormat)
-            {
+  public static String formatarDateddMMyyyyhhmmParaString(java.util.Date dateToFormat) {
 
+    return DD_MM_YYYY_HH_MM.format(dateToFormat);
+  }
 
+  public static String retirarBarrasDaDataNoPadraoddMMyyyyParaString(java.util.Date dateToFormat) {
 
-        return FORMATADOR_YYYY_MM_DD_HH_MM_SS.format(dateToFormat);
-    }
+    return DD_MM_YYYY.format(dateToFormat).replace("/", "");
+  }
 
-    public static String formatarDateddMMyyyyhhmmParaString(java.util.Date dateToFormat) {
+  public static Date converterStringParaDate(String dataSTR) throws ParseException {
 
-        return FORMATADOR_dd_MM_YYYY_hh_mm.format(dateToFormat);
-    }
+    return DD_MM_YYYY.parse(dataSTR);
+  }
 
-    public static String retirarBarrasDaDataNoPadraoddMMyyyyParaString(
-            java.util.Date dateToFormat) {
-
-        return FORMATADOR_dd_MM_YYYY.format(dateToFormat).replace("/", "");
-    }
-    public static Date converterStringParaDate(
-            String dataSTR) throws ParseException {
-
-        return FORMATADOR_dd_MM_YYYY.parse(dataSTR);
-    }
-
-    public static boolean ehUmPeriodoValido(Date dataInicial,Date dataFinal){
-        return dataInicial.before(dataFinal) || dataInicial.equals(dataFinal);
-    }
+  public static boolean ehUmPeriodoValido(Date dataInicial, Date dataFinal) {
+    return dataInicial.before(dataFinal) || dataInicial.equals(dataFinal);
+  }
 }
